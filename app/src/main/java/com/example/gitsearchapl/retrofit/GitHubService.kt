c54d1595f9ef78af02e00f2fbeb0b1ed79deb9c8
Dimon22854 <dimon3limon3@gmail.com>
2021-12-07 23:00:36 +0300

    retrofit cr

--- app/src/main/java/com/example/gitsearchapl/retrofit/GitHubService.kt
+++ app/src/main/java/com/example/gitsearchapl/retrofit/GitHubService.kt
@@ -5,11 +5,25 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.Retrofit
-
-
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 
 interface GitHubService {
     @GET("search/repositories?q={query}")
-    fun listRepos(@Path("user") user: String?): Call<MutableList<Repo?>?>?
+    fun listRepos(@Path("query") query: String?): Call<MutableList<Repo?>?>?
+
+    companion object {
+
+        var BASE_URL = "https://api.github.com/"
+
+        fun create() : GitHubService {
+
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(MoshiConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+            return retrofit.create(GitHubService::class.java)
+
+        }
+    }
 }
